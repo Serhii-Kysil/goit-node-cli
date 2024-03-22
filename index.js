2f600a57bbcaf87eb76d15760acadb7a12f54a21
Serhii-Kysil <serik.kisel000@gmail.com>
2024-03-22 10:26:40 +0200

    add task

---
+++ index.js
@@ -0,0 +1,37 @@
+import { program } from "commander";
+import * as contacts from "./data/contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+const opt = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      return console.log(contactsList);
+
+    case "get":
+      const getContact = await contacts.getContactById(id);
+      return console.log(getContact);
+
+    case "add":
+      const addContact = await contacts.addContact(name, email, phone);
+      return console.log(addContact);
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(opt);
